f6587409d0a519d6051da2f963ab8b6c52634440
josh-everett01 <59103702+josh-everett01@users.noreply.github.com>
2020-07-28 21:25:49 -0500

    Added fetch items option and insert items functionality  in UI

--- calorie_tracker/app.js
+++ calorie_tracker/app.js
@@ -22,6 +22,9 @@ const ItemCtrl = (function () {
 
   //Public Methods
   return {
+    getItems: function() {
+        return data.items;
+    },
     logData: function () {
       return data;
     }
@@ -32,10 +35,27 @@ const ItemCtrl = (function () {
 
 // UI Controller
 const UICtrl = (function () {
+    const UISelectors = {
+      itemList: '#item-list'
+    }
 
   // Public Methods
   return {
+    populateItemList: function(items){
+      let html = '';
 
+      items.forEach(function(item){
+        html += `<li class="collection-item" id="item-${item.id}">
+        <strong>${item.name}: </strong> <em>${item.calories} Calories</em>
+        <a href="" class="secondary-content">
+          <i class="edit-item fa fa-pencil"></i>
+        </a>
+      </li>`
+      });
+
+      // Insert List Items
+      document.querySelector(UISelectors.itemList).innerHTML = html;
+    }
   }
 })();
 
@@ -45,7 +65,11 @@ const App = (function (ItemCtrl, UICtrl) {
   // Public Methods
   return {
     init: function () {
-      console.log('Initializing App...');
+      // Fetch Items from Data Structure
+      const items = ItemCtrl.getItems();
+
+      // Populate List With Items
+      UICtrl.populateItemList(items);
     }
   }
 
